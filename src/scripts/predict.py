35b977ff97b03b70d456e11e351c40a5e2dd20a1
MS <124096953+MarianSlassi@users.noreply.github.com>
2025-07-21 18:55:18 +0300

    nano fix of logger in predfict.py

--- src/scripts/predict.py
+++ src/scripts/predict.py
@@ -8,6 +8,6 @@ from src.utils.logger import get_logger
 if __name__ == '__main__':
     # python -m src.scripts.predict
     config = Config()
-    logger_predict = get_logger("predict", log_file = config.get('xgb_model'))
+    logger_predict = get_logger("predict", log_file = config.get('log_file_model'))
     model = XGB_model(config, logger_predict)
     model.predict(load=True,save=True)
